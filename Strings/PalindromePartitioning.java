0ad5fb91d82e42e1655763fc9e47d00242296824
PrernaWagh <prernawagh006@gmail.com>
2026-03-26 12:32:19 +0530

    Palindrome partitioning done

---
+++ Strings/PalindromePartitioning.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        func(0,s,path,res);
+        return res;
+    }
+    void func(int index,String s,List<String> path,List<List<String>> res)
+    {
+        if(index == s.length())
+        {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=index;i<s.length();i++)
+        {
+            if(isPalindrome(s,index,i))
+            {
+                path.add(s.substring(index,i+1));
+                func(i+1,s,path,res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+    boolean isPalindrome(String s,int st,int end)
+    {
+        while(st <= end)
+        {
+            if(s.charAt(st++) != s.charAt(end--))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
