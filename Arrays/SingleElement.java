b1d4bf27ac12d9997d91584642081f71b4f479b6
PrernaWagh <prernawagh006@gmail.com>
2026-04-12 10:49:28 +0530

    Finding a single element in an array done

---
+++ Arrays/SingleElement.java
@@ -0,0 +1,33 @@
+public class SingleElement {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 1;
+        int high = n-2;
+        int mid;
+        if(n == 1)
+        {
+            return nums[0];
+        }
+        if(nums[0] != nums[1])
+        {
+            return nums[0];
+        }
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        while(low <= high)
+        {
+            mid = (low + high)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+            {
+                return nums[mid];
+            }
+            if(mid % 2 == 1 && nums[mid-1] == nums[mid] || mid % 2 == 0 && nums[mid + 1] == nums[mid])
+            {
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+}
