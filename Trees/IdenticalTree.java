10d810e27c66023a979b8c085cb7ba68f5e941a2
PrernaWagh <prernawagh006@gmail.com>
2026-05-08 23:09:33 +0530

    Identical trees problem done

---
+++ Trees/IdenticalTree.java
@@ -0,0 +1,31 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class IdenticalTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        boolean isLeftSame = isSameTree(p.left, q.left);
+        boolean isRightSame = isSameTree(p.right, q.right);
+
+        return isLeftSame && isRightSame && p.val == q.val;
+    }
+}
