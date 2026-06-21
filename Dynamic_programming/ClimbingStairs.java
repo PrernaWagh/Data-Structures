65e998311e87529667acf21663d2569c4d93af80
PrernaWagh <prernawagh006@gmail.com>
2026-06-21 16:51:34 +0530

    climbing stairs problem done

---
+++ Dynamic_programming/ClimbingStairs.java
@@ -0,0 +1,16 @@
+//dynamic programming approach
+class ClimbingStairs {
+    public int climbStairs(int n) {
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 0;
+
+        for(int i=2;i<=n;i++)
+        {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
